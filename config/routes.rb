ddca3ffe60c9adc78ccaea8f254c925c2a7e0e9e
Daniela Guardado <dmguardado@ipn.pt>
2017-12-11 22:37:43 +0000

    Pagina dos objetivos

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'eventos#index'
-
+  get "/pages/:page" => "pages#show"
   resources :eventos
 end
