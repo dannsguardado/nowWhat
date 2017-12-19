73adb27d36f157bb7eb017f880b285e7902f95df
Rita Almeida <ritaa@student.dei.uc.ptmkdir>
2017-12-19 00:51:08 +0000

    about

---
+++ app/assets/javascripts/obj.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function () {
+  $(".first").click(function () {
+    $(".text").replaceWith("Com esta feira planeamos dar a conhecer, aos estudantes da nossa faculdade, todas as hipóteses que têm ao seu dispor durante e após o seu percurso académico. Assim planeamos expor e clarificar o mundo do empreendedorismo, bem como promover uma interação com as várias estruturas empregadoras.");
+  });
+  $(".second").click(function () {
+    $(".text").replaceWith("-Estágios Nada melhor que a experiência, desta modo irá decorrer a promoção dos estágios que existem propostos na área da informática e do design e multimédia.Assim as entidades que nos visitarem poderão promover as suas propostas durante o evento e e breve também aqui!");
+  });
+  $(".third").click(function () {
+    $(".text").replaceWith("-Experiencia de trabalhar numa empresa Em breve muitos de nós terminarão o seu percurso académico sem saber o que esperar do mundo do trabalho.Pretendemos assim clarificar essa experiência, introduzindo diferentes áreas e métodos de trabalho");
+  });
+});
