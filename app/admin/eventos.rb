cd5e39096576f91f1a6dc841f29204da1a36596b
Daniela Guardado <dmguardado@ipn.pt>
2018-02-26 00:35:34 +0000

    Active admin for eventos + navbar correction

--- app/admin/eventos.rb
+++ app/admin/eventos.rb
@@ -3,7 +3,20 @@ ActiveAdmin.register Evento do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
-#
+permit_params :name, :tipo, :date, :theme, :description
+
+
+
+form do |f|
+  f.inputs do
+      f.input :name
+      f.input :tipo
+      f.input :date
+      f.input :theme
+      f.input :description
+    f.submit
+  end
+end
 # or
 #
 # permit_params do
