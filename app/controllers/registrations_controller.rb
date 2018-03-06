7724aaa56807de933c0b98797128692a8ffe66fa
Daniela Guardado <dmguardado@ipn.pt>
2018-03-06 02:52:05 +0000

    Profile create correction

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,11 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    new_perfil_path # Or :prefix_to_your_route
+  	@perfil = Perfil.create(nome: "Nome", instituto: "Instituto", curso: "Curso")     
+  	@perfil.user = current_user
+  	 @perfil.save
+  	new_perfil_path # Or :prefix_to_your_route
+
+  	#current_user.perfil
   end
 end
